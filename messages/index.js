8eabf3747aa6b0ca933d7abb527b1b3c242a7e34
vkacherov <vkacherov@gmail.com>
2016-11-29 16:17:48 -0500

    Init commit

---
+++ messages/index.js
@@ -0,0 +1,70 @@
+/*-----------------------------------------------------------------------------
+This template gets you started with a simple dialog that echoes back what the user said.
+To learn more please visit
+https://docs.botframework.com/en-us/node/builder/overview/
+-----------------------------------------------------------------------------*/
+"use strict";
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
+var azure = require('azure-storage');
+
+var useEmulator = (process.env.NODE_ENV == 'development');
+
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    stateEndpoint: process.env['BotStateEndpoint'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+});
+
+var bot = new builder.UniversalBot(connector);
+
+// Intercept trigger event (ActivityTypes.Trigger)
+bot.on('trigger', function (message) {
+    // handle message from trigger function
+    var queuedMessage = message.value;
+    var reply = new builder.Message()
+        .address(queuedMessage.address)
+        .text('This is coming from the trigger: ' + queuedMessage.text);
+    bot.send(reply);
+});
+
+// Handle message from user
+bot.dialog('/', function (session) {
+    var queuedMessage = { address: session.message.address, text: session.message.text };
+    // add message to queue
+    session.sendTyping();
+    var queueSvc = azure.createQueueService(process.env.AzureWebJobsStorage);
+    queueSvc.createQueueIfNotExists('bot-queue', function(err, result, response){
+        if(!err){
+            // Add the message to the queue
+            var queueMessageBuffer = new Buffer(JSON.stringify(queuedMessage)).toString('base64');
+            queueSvc.createMessage('bot-queue', queueMessageBuffer, function(err, result, response){
+                if(!err){
+                    // Message inserted
+                    session.send('Your message (\'' + session.message.text + '\') has been added to a queue, and it will be sent back to you via a Function');
+                } else {
+                    // this should be a log for the dev, not a message to the user
+                    session.send('There was an error inserting your message into queue');
+                }
+            });
+        } else {
+            // this should be a log for the dev, not a message to the user
+            session.send('There was an error creating your queue');
+        }
+    });
+
+});
+
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());    
+} else {
+    module.exports = { default: connector.listen() }
+}
+
+
